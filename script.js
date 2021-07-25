72757947c3850526b05e1012fd5703822733e69c
imildositoe <imildositoe@gmail.com>
2021-07-25 23:24:22 +0200

    all promises added on script.js file

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const video = document.getElementById('video');
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
+
 function startVideo() {
     navigator.getUserMedia(
         {video: {}},
         stream => video.srcObject = stream,
-        err => console.log(err)
+        err => console.error(err)
     );
 }
 
