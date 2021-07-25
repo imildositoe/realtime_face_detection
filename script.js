a8fe8db14084bd537cdbc7368f7e414fff043bc8
imildositoe <imildositoe@gmail.com>
2021-07-25 23:10:24 +0200

    listener to play webcam video added

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function startVideo() {
         err => console.log(err)
     );
 }
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = {width: video.width, height: video.height};
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const resisedDetections = faceapi.resizeResults(detections, displaySize);
+    }, 100);
+});
