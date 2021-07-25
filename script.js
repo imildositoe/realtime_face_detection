ee08e9c60daf7404e3051df7519c5e566011f972
imildositoe <imildositoe@gmail.com>
2021-07-25 23:46:44 +0200

    detection, faceExpression methods added

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function startVideo() {
     );
 }
 
-video.addEventListener('play', () => {
+video.addEventListener('playing', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
     const displaySize = {width: video.width, height: video.height};
@@ -23,5 +23,9 @@ video.addEventListener('play', () => {
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     }, 100);
 });
