92bc16e92bc824aeb3916789ac1fbdd3af3d1416
imildositoe <imildositoe@gmail.com>
2021-07-26 00:12:17 +0200

    listener type changed to play

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function startVideo() {
     );
 }
 
-video.addEventListener('playing', () => {
+video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
     const displaySize = {width: video.width, height: video.height};
